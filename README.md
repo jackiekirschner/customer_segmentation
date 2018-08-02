403ca04875b6f7eeef3573f8910107b20cb7d8b7
Jacqueline Kirschner <jacquelinekirschner@Jacquelines-MacBook-Pro.local>
2018-08-02 17:46:25 -0600

    Thursday updates

--- README.md
+++ README.md
@@ -1,1 +1,100 @@
-# customer_segmentation
+# Ecommerce Traffic Segmentation
+## Unsupervised Learning – Clustering
+
+<img src= 'images/merch_store.png'>
+
+### Question
+Can website sessions be clustered by how and when users arrive at the site? If so, how can clusters be used to inform marketing decisions?  
+
+### Data
+I used data from the Google Merchandise online store. https://shop.googlemerchandisestore.com/ Google Analytics data from August 1, 2016 - August 1, 2017 (>200,000 web sessions) is publicly available on Google Big Query. I used SQL to query 4 months of website sessions, where one row represents one session.
+
+<img src= 'images/big_query.png'>
+```sql
+sql query here
+```
+
+### Features
+* Number of site visits – new or returning visitor
+* Device – desktop or mobile
+* Traffic medium – organic, paid, referral or affiliate
+* Time of day – morning, afternoon, evening or night
+
+### Clustering
+Sklearn's Kprototypes is a combination of KMeans and KModes. It's used for clustering a mix of numeric and categorical features. When fitting, KPrototypes takes the categorical indicies as an argument and clusters accordingly.
+
+```python
+def get_labels(k):
+  init = 'Huang'
+  n_clusters = 8
+  max_iter = 100
+
+  kproto = kprototypes.KPrototypes(n_clusters=k,init=init, max_iter=max_iter)
+
+  categoricals_indicies = [1,2,3,4,5,6,7,8,9]
+  labels = kproto.fit_predict(X_scaled, categorical=categoricals_indicies)
+  return labels
+```
+
+### Picking Ideal # of Clusters
+
+##### Elbow
+<img src= 'images/thursday_elbow_plot.png' width="800">
+
+##### Principle Component Analysis
+<img src = 'images/8_3D_PCA.png' width="400" height="350">
+<img src= 'images/9_3D_PCA.png' width="400" height="350">
+
+##### Silouette
+<img src= 'images/thurs_silouette_score.png' width="400">
+
+
+### Cluster Features
+To interpret feature information about each cluster, I added a labels column to the original features dataframe, grouped by cluster, and aggregated column-wise by the mean.
+
+| Cluster | # of visits | social referral | device | organic | paid | referral/affiliate | night | morning | afternoon | evening |
+| -- |:----:| ---:|----: |:----:| ---:|----: |:---:| ----:|:---:| ----:|
+|1| 1.504 | 0.0  | 0.263 |1.0| 0.0 |0.0 | 0.0| 0.0| 0.0 | 1.0|
+|2| 157.637  | 0.0 |0.0| 0.400 |0.0 | 0.600  |0.200  |0.0 | 0.600 | 0.200 |
+|3| 26.387| 0.027| 0.041 | 0.257 | 0.041| 0.703 | 0.176 | 0.0270  | 0.338 | 0.392
+|4| 1.506 |  0.0 | 0.342 |1.00 | 0.0| 0.0 | 0.413  |0.101 |  0.397 | 0.0
+|5| 1.855 | 0.0  | 0.387 | 0.0 | 1.0  | 0.0 | 0.266 | 0.0840  | 0.248|  0.344
+|6| 2.260 | 0.045| 0.019 | 0.0| 0.0|1.0 | 0.0 | 0.0 | 0.0 | 1.0
+|7| 2.046 | 0.054| 0.031 | 0.0 | 0.0 |1.0 | 0.0 | 0.068  | 0.822 | 0.0
+|8| 2.644 | 0.097| 0.0517 | 0.011 | 0.0 | 0.990 | 0.897  |0.028 |  0.0 | 0.0
+
+
+### Interpretation
+Below is a qualitative interpretation of cluster features based on the means of each column.
+
+| Cluster | Size  |# of visits | social referral | device | traffic medium | time of day |
+| -- |:----:| ---:| ---:|----: |:----:| ---:|
+|1| 1079 |New Visitor | No | Some Mobile | Organic| Evening|
+|2| 5 |Very Frequent Visitor | No | Desktop| Organic & Ref/Affil  | Afternoon |
+|3| 74 |Frequent Visitor | No | Desktop | Organic & Ref/Affil | Afternoon & Evening |
+|4| 2120 | New Visitor |  No | Some Mobile | Organic | Night & Afternoon |
+|5| 488 |New Visitor | No  | Some Mobile | Paid | Heavy Evening
+|6| 1434 |Second Visit | No | Desktop | Ref/Affil | Evening |
+|7| 1271 |Second Visit | No | Desktop | Ref/Affil | Afternoon |
+|8| 777 |2-3 Visits | Some  | Desktop | Ref/Affil | Night |
+
+### Clustering Insights
+
+### Cluster Performance Insights
+To see how each cluster performed on the site, I created a 'Y' dataframe with some additional columns.
+
+```python
+Y['conversion_rate'] = Y['num_transactions'] / Y['num_visits']
+Y['avg_conversion_value'] = Y['revenue'] / Y['num_transactions']
+```
+* paid traffic leans mobile
+* new visitors lean mobile (except for paid traffic), return visitors lean desktop
+*
+
+### EDA
+
+* afternoon browsers spend more time on site
+
+### Future Work
+
+### Conclusion
--- src/wednesday.py
+++ src/wednesday.py
@@ -14,17 +14,17 @@ from numpy.random import seed
 seed(5)
 
 # read in data
-dfWorking = pd.read_csv('../data/USsessions.csv')
+dfQuery = pd.read_csv('../data/USsessions.csv')
 
 '''Data Cleaning'''
 # clean columns
-dfWorking['time_on_site'] = dfWorking['time_on_site'] / 60
-dfWorking['revenue'] = dfWorking['revenue'] / (10**6)
-dfWorking['social_referral'] = dfWorking.social_referral.map(dict(Yes=1, No=0))
-dfWorking['device'] = dfWorking.device.map(dict(desktop=0, mobile=1, tablet=1))
-dfWorking['time_of_visit'] = pd.to_datetime(dfWorking['time_of_visit'],unit='s')
-dfWorking = dfWorking.fillna(0)
-dfWorking = dfWorking.drop(columns=['date'])
+dfQuery['time_on_site'] = dfQuery['time_on_site'] / 60
+dfQuery['revenue'] = dfQuery['revenue'] / (10**6)
+dfQuery['social_referral'] = dfQuery.social_referral.map(dict(Yes=1, No=0))
+dfQuery['device'] = dfQuery.device.map(dict(desktop=0, mobile=1, tablet=1))
+dfQuery['time_of_visit'] = pd.to_datetime(dfQuery['time_of_visit'],unit='s')
+dfQuery = dfQuery.fillna(0)
+dfQuery = dfQuery.drop(columns=['date'])
 
 # adjust time of visit to local time
 PST=['Pleasanton', 'Tigard', 'Westlake Village', 'Hayward', 'Redwood City', 'San Mateo', 'Santa Ana', 'Bellevue', 'Fremont', 'Palo Alto', 'Cupertino', 'San Bruno', 'Sunnyvale', 'Milpitas', 'San Jose', 'Bothell', 'Irvine', 'Portland', 'Oakland',
@@ -41,127 +41,144 @@ EST=['Goose Creek','Akron', 'Wellesley', 'Cincinnati', 'Chamblee', 'Lenoir', 'Je
 def offset(city):
     return 3600 * (-8 * (city in PST) - 7 * (city in MST) - 6 * (city in CST)  - 5 * (city in EST))
 
-dfWorking['local_time'] = pd.to_datetime(dfWorking['city'].map(offset) + dfWorking['time_of_visit'], unit='s')
+dfQuery['local_time'] = pd.to_datetime(dfQuery['city'].map(offset) + dfQuery['time_of_visit'], unit='s')
 
 # create 'time of day' categorical column from local time
-dfWorking = dfWorking.assign(time_of_day=pd.cut(dfWorking.local_time.dt.hour,[0,6,12,18,24],labels=['Night','Morning','Afternoon','Evening']))
+dfQuery = dfQuery.assign(time_of_day=pd.cut(dfQuery.local_time.dt.hour,[0,6,12,18,24],labels=['Night','Morning','Afternoon','Evening']))
 
-# features for clustering
-X = dfWorking[['num_visits', 'social_referral', 'device']]
+
+'''Features for Clustering'''
+X = dfQuery[['num_visits', 'social_referral', 'device']]
 
 # get dummies for 'traffic medium'
-dfMediums = pd.get_dummies(dfWorking["traffic_medium"])
-#combine two paid traffic columns
+dfMediums = pd.get_dummies(dfQuery["traffic_medium"])
+# combine two paid traffic columns
 dfMediums['paid'] = dfMediums['cpc']+dfMediums['cpm']
-dfMediums = dfMediums.drop(columns=['cpc', 'cpm'])
 # combine two traffic link columns
 dfMediums['referral_affiliate'] = dfMediums['referral']+dfMediums['affiliate']
-dfMediums = dfMediums.drop(columns=['referral', 'affiliate'])
+# drop now-combined columns
+dfMediums = dfMediums.drop(columns=['referral', 'affiliate', 'cpc', 'cpm'])
 
 # get dummies for time of day
-dftod = pd.get_dummies(dfWorking["time_of_day"])
+dftod = pd.get_dummies(dfQuery["time_of_day"])
 
-# join X and dummied features
-dfSessions =  pd.concat([X, dfMediums, dftod], axis=1)
-
+# join X and dummied features for final feature matrix
+X =  pd.concat([X, dfMediums, dftod], axis=1)
 
 # standardize
 scaler = StandardScaler()
-scaled = scaler.fit_transform(dfSessions)
+X_scaled = scaler.fit_transform(X)
 
 '''Clustering'''
 # k-prototypes (mixed numeric and categorical features)
 init = 'Huang'
-n_clusters = 5
+n_clusters = 9
 max_iter = 100
 
 kproto = kprototypes.KPrototypes(n_clusters=n_clusters,init=init, max_iter=max_iter)
 
 # fit/predict
 categoricals_indicies = [1,2,3,4,5,6,7,8,9]
-labels = kproto.fit_predict(scaled, categorical=categoricals_indicies)
+labels = kproto.fit_predict(X_scaled, categorical=categoricals_indicies)
 
-# copy features datafrme, add column with predicted cluster labels
-dfcopy = dfSessions.copy(deep=True)
-dfcopy['cluster'] = labels
+# add column to original feature matrix with predicted cluster labels
+X['cluster'] = labels
 
-# interpret clusters by taking mean
-means = dfcopy.groupby(['cluster']).mean()
-
-#
-dfY =  pd.concat([dfWorking, dfMediums, dftod], axis=1)
-dfY.drop(columns=['user_id', 'time_of_visit', 'traffic_medium',
-       'traffic_source', 'social_referral', 'device', 'local_time', 'time_of_day', 'organic', 'paid',
-       'referral_affiliate', 'Night', 'Morning', 'Afternoon', 'Evening'], inplace=True)
-dfY['cluster'] = labels
-dfCounts = dfY.cluster.value_counts()
-
-dfY = dfY.groupby('cluster').agg({'page_views':'mean','time_on_site':'mean', 'num_visits': 'sum',
-'num_transactions':'sum','revenue': 'sum', 'action_type': 'mean'})
-
-dfY['size'] = dfCounts.iloc[:]
-
-dfY['conversion_rate'] = dfY['num_transactions'] / dfY['num_visits']
-dfY['avg_conversion_value'] = dfY['revenue'] / dfY['num_transactions']
+# interpret clusters by taking column-wise mean of features
+means = X.groupby(['cluster']).mean()
 
 '''Ideal K'''
 
 # Elbow plot
-distorsions = []
-for k in range(2, 10):
-    kprot = kprototypes.KPrototypes(n_clusters=k)
-    kprot.fit(scaled, categorical=categoricals_indicies)
-    distorsions.append(kprot.cost_)
+# distorsions = []
+# for k in range(2, 11):
+#     kprot = kprototypes.KPrototypes(n_clusters=k)
+#     kprot.fit(X_scaled, categorical=categoricals_indicies)
+#     distorsions.append(kprot.cost_)
+#
+# fig = plt.figure(figsize=(15, 5))
+# plt.plot(range(2, 11), distorsions)
+# plt.grid(True)
+# plt.xlabel('K')
+# plt.ylabel('Sum Distance to Centroids')
+# plt.title('Elbow curve')
+# plt.savefig('../images/thursday_elbow_plot.png')
+# plt.show()
 
-fig = plt.figure(figsize=(15, 5))
-plt.plot(range(2, 10), distorsions)
-plt.grid(True)
-plt.xlabel('K')
-plt.ylabel('Sum Distance to Centroids')
-plt.title('Elbow curve')
-# plt.savefig('wed_elbow_plot.png')
-plt.show()
-
-# Silouette Score
+# # Silouette Score
 # def get_silhouette_score(nclust):
 #     kprot = kprototypes.KPrototypes(nclust)
-#     labels = kprot.fit_predict(scaled,categorical=categoricals_indicies)
-#     sil_avg = silhouette_score(scaled, labels)
+#     labels = kprot.fit_predict(X_scaled, categorical=categoricals_indicies)
+#     sil_avg = silhouette_score(X_scaled, labels)
 #     return sil_avg
-#
+# #
 # sil_scores = [get_silhouette_score(i) for i in range(2,11)]
 # plt.plot(range(2,11), sil_scores)
 # plt.grid(True)
 # plt.xlabel('K')
 # plt.ylabel('Silhouette Score')
 # plt.title('Silhouette Score vs K')
-# plt.savefig('wed_silouette_score.png')
+# plt.savefig('../images/thurs_silouette_score.png')
 # plt.show()
 
 
 
 '''Data Visualiztion'''
-dfcopy['cluster']
 
-# 2D
-# Transform df into two features with PCA
+# # 2D
+# # Transform df into two features with PCA
 # pca = PCA(2)
-# plot_columns = pca.fit_transform(scaled)
+# plot_columns = pca.fit_transform(X_scaled)
 # # Plot based on two dimensions, and shade by cluster label
-# plt.scatter(x=plot_columns[:,0], y=plot_columns[:,1], c=dfcopy['cluster'], s=30)
-# plt.title('PCA (2 Components) Feature Visualization')
-# plt.savefig('wed_2D_PCA.png')
+# fig, ax = plt.subplots()
+# ax.scatter(x=plot_columns[:,0], y=plot_columns[:,1], c=labels,cmap='nipy_spectral', s=30)
+# ax.set_alpha(0.0001)
+# plt.title('K=8 Feature Visualization')
+# plt.savefig('../images/9_2D_PCA.png')
 # plt.show()
 
-# 3D
-# Transform df into three features with PCA
+# # 3D
+# # Transform df into three features with PCA
 # pca = PCA(3)
-# plot_columns = pca.fit_transform(scaled)
+# plot_columns = pca.fit_transform(X_scaled)
 # # Plot based on three dimensions, and shade by cluster label
 # fig = plt.figure()
 # ax = plt.axes(projection='3d')
-# ax.scatter3D(plot_columns[:,0], plot_columns[:,1], plot_columns[:,2], c=dfcopy['cluster'], s=30)
+# ax.scatter3D(plot_columns[:,0], plot_columns[:,1], plot_columns[:,2], c=labels, cmap='nipy_spectral',s=30)
 # ax.set_alpha(0.0001)
-# plt.title('PCA (3 Components) Feature Visualization')
-# plt.savefig('wed_3D_PCA.png')
+# plt.title('K=9 Feature Visualization')
+# plt.savefig('../images/9_3D_PCA.png')
 # plt.show()
+
+'''Features EDA'''
+# % mobile vs. Desktop
+# %
+
+'''EDA by Cluster'''
+# create 'Y' table to see cluster performance on site
+Y = dfQuery[['page_views', 'time_on_site', 'num_visits', 'num_transactions', 'revenue','action_type']]
+Y['cluster'] = labels
+
+cluster_sizes = Y.cluster.value_counts()
+
+Y = Y.groupby('cluster').agg({'page_views':'mean','time_on_site':'mean', 'num_visits': 'sum',
+'num_transactions':'sum','revenue': 'sum', 'action_type': 'mean'})
+
+Y['conversion_rate'] = Y['num_transactions'] / Y['num_visits']
+Y['conversion_value'] = Y['revenue'] / Y['num_transactions']
+
+Y['size'] = cluster_sizes.iloc[:]
+
+cluster_names = []
+
+# plot conversion rate by cluster
+cr = Y.xs('conversion_rate', axis=1)
+cr.plot(kind='bar', title='Conversion Rate')
+plt.savefig('../images/conversion_rate.png')
+plt.show()
+
+# conversion value by cluster
+cv = Y.xs('conversion_value', axis=1)
+cv.plot(kind='bar', title='Conversion Value')
+plt.savefig('../images/conversion_value.png')
+plt.show()
